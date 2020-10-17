6e2fa95d88f4da9a71abeef59c1782935cc4bdc1
poignant-semicolon-2878 <jndossantos00@gmail.com>
2020-10-17 22:41:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,11 +1,8 @@
 def say_hello(name="Gabriela")
   puts "Hello #{name}!"
-end"
-say_hello
-Hello Gabriela!
+end
+
 
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
 end
-say_hello
-Hello Ruby Programmer
